58f2fd486ffb633e414da5a758ea6074b3b76ad6
林韋廷 <alan885544@gmaiil.com>
2023-03-03 22:27:48 +0800

    git add

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
     })
   ],
+  base: '/vite-deploy-sample3/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
